9b82c7cfd4a6843a5d7c59bb494a1645650e06e9
Paulo Sousa <prsousa34@gmail.com>
2016-06-26 15:16:41 +0100

    Added support to OpenCV

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+
+void displayImage(cv::Mat img) {
+    if( img.rows > 1000 ) {
+        cv::resize(img, img, cv::Size(img.cols / 2, img.rows / 2));
+    }
+
+    cv::namedWindow("Segments", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Segments", img);
+    cv::waitKey(0);
 }
 
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        cerr << "Error: Image not specified!" << endl;
+        return EXIT_FAILURE;
+    }
+
+    string filename = argv[1];
+    cv::Mat img = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
+    if (img.empty()) {
+        cerr << "Error: Image cannot be loaded!\n" << endl;
+        return EXIT_FAILURE;
+    }
+
+    // code here
+
+    displayImage(img);
+
+    return 0;
+}
