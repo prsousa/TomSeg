4e1458a7a496dacb98835322a4e65c8a5e2fce31
Paulo Sousa <prsousa34@gmail.com>
2016-12-07 09:39:34 +0000

    Add time measurement to CUDA code

--- segmentation-manager/segmentors/cuda-kernels.cu
+++ segmentation-manager/segmentors/cuda-kernels.cu
@@ -1,6 +1,12 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+#define CUDA_DEBUG 1
+
+#if CUDA_DEBUG
+#include <stdio.h>
+#endif
+
 __global__
 void erode_GPU( const unsigned char *labels, unsigned char *res, int size, int width, int height ) {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
@@ -53,18 +59,63 @@ void erodeAndDilate_GPU( unsigned char* labels, int size, int width, int height 
     unsigned char  *labels_gpu, *res;
     int sizeLabels_bytes = width * height * int(sizeof(unsigned char));
 
+#if CUDA_DEBUG
+    float millisecondsSending = 0;
+    float millisecondsKernel = 0;
+    float millisecondsReceiving = 0;
+    float milliseconds = 0;
+    cudaEvent_t start, stop, startSending, stopSending, startKernel, stopKernel, startReceiving, stopReceiving;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventCreate(&startSending);
+    cudaEventCreate(&stopSending);
+    cudaEventCreate(&startKernel);
+    cudaEventCreate(&stopKernel);
+    cudaEventCreate(&startReceiving);
+    cudaEventCreate(&stopReceiving);
+
+    cudaEventRecord(start);
+#endif
     cudaMalloc((void**) &labels_gpu, sizeLabels_bytes);
     cudaMalloc((void**) &res, sizeLabels_bytes);
-
+#if CUDA_DEBUG
+    cudaEventRecord(startSending);
+#endif
     cudaMemcpy(labels_gpu, labels, sizeLabels_bytes, cudaMemcpyHostToDevice);
+#if CUDA_DEBUG
+    cudaEventRecord(stopSending);
+    cudaEventSynchronize(stopSending);
+    cudaEventElapsedTime(&millisecondsSending, startSending, stopSending);
 
+    cudaEventRecord(startKernel);
+#endif
     size_t numThreads = 512;
     size_t numBloks = (width*height) / numThreads;
     erode_GPU<<<numBloks, numThreads>>>(labels_gpu, res, size, width, height);
     dilate_GPU<<<numBloks, numThreads>>>(res, labels_gpu, size, width, height);
+#if CUDA_DEBUG
+    cudaEventRecord(stopKernel);
+    cudaEventSynchronize(stopKernel);
+    cudaEventElapsedTime(&millisecondsKernel, startKernel, stopKernel);
 
+    cudaEventRecord(startReceiving);
+#endif
     cudaMemcpy(labels, labels_gpu, sizeLabels_bytes, cudaMemcpyDeviceToHost);
-
+#if CUDA_DEBUG
+    cudaEventRecord(stopReceiving);
+    cudaEventSynchronize(stopReceiving);
+    cudaEventElapsedTime(&millisecondsReceiving, startReceiving, stopReceiving);
+#endif
     cudaFree(labels_gpu);
     cudaFree(res);
+#if CUDA_DEBUG
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    printf("Sending Time:\t%f ms\n", millisecondsSending);
+    printf("Kernel Time:\t%f ms\n", millisecondsKernel);
+    printf("Receiving Time:\t%f ms\n", millisecondsReceiving);
+    printf("Overall Time:\t%f ms\n", milliseconds);
+#endif
 }
